48cebccb9bc102457fcc8c2c52f3a34fdf8ee988
Matthew Li <matthewli2003@gmail.com>
2023-06-13 20:46:59 -0700

    Added quicksort folder as well as updated CmakeList.txt

---
+++ sorting/quicksort/Test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Matthew Li on 6/13/23.
+//
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+    int a[4] = {0, 1, 2, 3};
+    for(int i = 0; i<4; i++) {
+        cout<<a[i]<<endl;
+    }
+    cout<<"ended"<<endl;
+}
